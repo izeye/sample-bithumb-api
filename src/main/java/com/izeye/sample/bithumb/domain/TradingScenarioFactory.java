477006cfba447ef2b016002b9d08576cd3292e50
Johnny Lim <izeye@naver.com>
2018-03-03 22:12:01 +0900

    Upgrade to Spring Boot 2.0.0.RELEASE

--- src/main/java/com/izeye/sample/bithumb/domain/TradingScenarioFactory.java
+++ src/main/java/com/izeye/sample/bithumb/domain/TradingScenarioFactory.java
@@ -10,7 +10,7 @@ public final class TradingScenarioFactory {
 	public static TradingScenario[] createLinearScenarios(CryptoCurrency currency, int start, int end) {
 		TradingScenario[] scenarios = new TradingScenario[end - start + 1];
 		for (int i = start; i <= end; i++) {
-			scenarios[i - start] = new TradingScenario(currency, i, i);
+			scenarios[i - start] = new TradingScenario(currency, i * 10, i);
 		}
 		return scenarios;
 	}
